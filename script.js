1ac0c478dce3ab3005f9545a510d9c2b1aa21754
Дарья <133729386+yungpinkby@users.noreply.github.com>
2023-09-20 12:34:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let loginBtn = document.getElementById("loginBtn");
+let loginForm = document.getElementById("loginForm");
+
+loginBtn.addEventListener("click", function() {
+
+  let isHidden = loginForm.style.display === "none";
+  
+  if (isHidden) {
+    loginForm.style.display = "block";
+  } else {
+    loginForm.style.display = "none";
+  }
+});
+
+let closeButton = document.getElementById("closeButton");
+
+closeButton.addEventListener("click", function() {
+  loginForm.style.display = "none";
+});
+
+function showRegistration() {
+  var registrationWindow = document.getElementById("registrationWindow");
+  registrationWindow.style.display = "block";
+}
