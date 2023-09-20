e67316f10050523238184870c4f3dd07546d24fc
Ivan Bunin <chernobelenkiy@gmail.com>
2023-09-20 12:56:09 +0300

    refactor to module pattern

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-let loginBtn = document.getElementById("loginBtn");
-let loginForm = document.getElementById("loginForm");
-
-loginBtn.addEventListener("click", function() {
-
-  let isHidden = loginForm.style.display === "none";
-  
-  if (isHidden) {
-    loginForm.style.display = "block";
-  } else {
-    loginForm.style.display = "none";
-  }
-});
-
-let closeButton = document.getElementById("closeButton");
-
-closeButton.addEventListener("click", function() {
-  loginForm.style.display = "none";
-});
-
-function showRegistration() {
+(function() {
+  let loginBtn = document.getElementById("loginBtn");
+  let loginForm = document.getElementById("loginForm");
+  let closeButton = document.getElementById("closeButton");
   let registrationWindow = document.getElementById("registrationWindow");
-  registrationWindow.style.display = "block";
-}
+
+  function init() {
+    loginBtn = document.getElementById("loginBtn");
+    loginForm = document.getElementById("loginForm");
+    closeButton = document.getElementById("closeButton");
+  }
+
+  function addEvents() {
+    loginBtn.addEventListener("click", function() {
+      const isHidden = loginForm.style.display === "none";
+      
+      if (isHidden) {
+        loginForm.style.display = "block";
+      } else {
+        loginForm.style.display = "none";
+      }
+    });
+
+    closeButton.addEventListener("click", function() {
+      loginForm.style.display = "none";
+    });
+  }
+
+  
+
+  function showRegistration() {
+    registrationWindow = document.getElementById("registrationWindow");
+    registrationWindow.style.display = "block";
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    init();
+    addEvents();
+  });
+})();
+
